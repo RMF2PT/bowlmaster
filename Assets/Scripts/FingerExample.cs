53d3136eff120fc8df9d74eceab611f992ab85d8
Ricardo Ferreira <ricardomfferreira@gmail.com>
2015-11-21 22:10:49 +0000

    - Created example of a finger launching the ball, if the game encounters a failed launch; - If ball's launch velocity is inferior to a certain limit, ball resets and shows the animation of a finger on the screen; - Added a failure check to see if the speed of the ball is inferior to 0, causing it to launch backwards; - Created IsSwiperMoving variable to avoid launching the ball when the swiper is moving.

---
+++ Assets/Scripts/FingerExample.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FingerExample : MonoBehaviour {
+
+	private Animator animator;
+	private float clipLenght;
+	private Vector3 StartPos;
+	private Ball ball;
+	
+	void Start () {
+		animator = GetComponent<Animator>();
+		gameObject.SetActive (false);
+		StartPos = transform.position;
+		ball = GameObject.FindObjectOfType<Ball>();
+	}
+	
+	public void LaunchFingerAnimation () {
+		gameObject.SetActive (true);
+		animator.SetTrigger("LaunchExampleTrigger");
+		Invoke("ResetAnimation", 2f);
+	}
+	
+	private void ResetAnimation () {
+		transform.position = StartPos;
+		gameObject.SetActive (false);
+		ball.inPlay = false;
+	}
+}
