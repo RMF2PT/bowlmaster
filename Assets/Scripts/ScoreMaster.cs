c7049060f14c3f2f72f39f75af369464406799ab
Ricardo Ferreira <ricardomfferreira@gmail.com>
2015-12-14 22:33:21 +0000

    Seen the solution of another student - Martin Beierl. I think I'm moving on.

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -87,6 +87,58 @@ public class ScoreMaster {
 				}
 			}
 		} 
+
+
+//		// Solution from Martin Beierl 
+//
+//		List<int> frameList = new List<int>();
+// 
+//        for (int i = 0; i < rolls.Count; i += 2)
+//        {
+//            // test if the frame is completed, ergo we have a second roll
+//            if (i != rolls.Count - 1)
+//            {
+//                int frameScore;
+//                int first = rolls[i];
+//                int second = rolls[i + 1];
+// 
+//                // Rolled a strike
+//                if (first == 10)
+//                {
+//                    // "second" is from next frame here (first bonux from the strike), 
+//					  // only addable if i+2 already exists, because this is the 2nd bonus from the strike
+//                    if (i + 2 < rolls.Count)
+//                    {
+//                        frameScore = 10 + second + rolls[i + 2];
+//                        frameList.Add(frameScore);
+// 
+//                        // if this was last frame, break out
+//                        if (frameList.Count == 10)
+//                            break;
+//                        else
+//                            i--; // because only one roll was made for a strike
+//                    }
+//                }
+//                // Rolled a spare
+//                else if (first + second == 10)
+//                {
+//                    // only addable if i+2 already exists, because this is the bonus for the spare
+//                    if (i + 2 < rolls.Count)
+//                    {
+//                        frameScore = 10 + rolls[i + 2];
+//                        frameList.Add(frameScore);
+//                    }
+//                }
+//                // Rolled nothing special
+//                else
+//                {
+//                    // add the two scores together
+//                    frameScore = first + second;
+//                    frameList.Add(frameScore);
+//                }
+//            }
+//        }
+
 		return frameList;
 	}
 }
