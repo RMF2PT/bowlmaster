681509d673a15786fe72d16d7291ede0f6a8ac50
Ricardo Ferreira <ricardomfferreira@gmail.com>
2015-12-15 21:20:34 +0000

    Finished ScoreMaster with code from Martin Beierl. Got to move on...

--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -22,72 +22,109 @@ public class ScoreMaster {
 	// Returns a list of individual frame scores, NOT cumulative
 	public static List<int> ScoreFrames (List<int> rolls) {
 		List<int> frameList = new List<int>();
-		int rollNum = 0;
-		int total = 0;
-		int spareTotal = 0;
-		int strikeTotal = 0;
-		int strikeSequence = 2;
-		bool isStrike = false;
-		
-		
-		foreach (int roll in rolls) {
-			rollNum++;
+				
+		for (int bowl = 0; bowl < rolls.Count; bowl += 2) {
+			// test if the frame is completed, we have a second roll
+			if (bowl != rolls.Count -1) {
+				int frame;
+				int firstBowl = rolls[bowl];
+				int secondBowl = rolls[bowl + 1];
 
-			if (rollNum > 21) {
-				if (strikeTotal > 0) {
-					strikeTotal += roll;
-					frameList.Add (strikeTotal);
-				} else {
-					spareTotal += roll;
-					frameList.Add (spareTotal);
-				}
-				continue;
-			}
+				if (firstBowl == 10) { // Strike
+					// "second" is from next frame here (first bonus from the strike), 
+					// can only add if i+2 already exists, because this is the 2nd bonus from the strike
+					if (bowl + 2 < rolls.Count) {
+						frame = firstBowl + secondBowl + rolls[bowl + 2];
+						frameList.Add (frame);
 
-			if (roll == 10) {
-				rollNum++;
-				isStrike = true;
-				if (strikeSequence > 0) {
-					strikeSequence--;
-					strikeTotal += roll;
-					continue;
-				}
-			}
-			
-			if (isStrike) {
-				strikeTotal += roll;
-				if (strikeSequence == 0) {
-					frameList.Add (strikeTotal);
-					strikeTotal -= 10;
-				}
-			}
-			
-			if (spareTotal > 0 && !isStrike) {
-				spareTotal += roll;
-				frameList.Add (spareTotal);
-				spareTotal = 0;
-			}
-			
-			total += roll;
-			
-			if (rollNum % 2 == 0) {
-				if (total % 10 == 0 && total > 0) {
-					spareTotal += total;
-					total = 0;
-				} else {
-					if (strikeTotal >= 10) {
-						frameList.Add (strikeTotal);
-						strikeTotal -= 10;
-						isStrike = false;
+						// If last frame, break out
+						if (frameList.Count == 10) {
+							break;
+						} else {
+							bowl--; // because only one roll was made for a strike
+						}
 					}
-					frameList.Add (total);
-					strikeTotal = 0;
-					strikeSequence = 2;
-					total = 0;
+				} else if (firstBowl + secondBowl == 10) { // Spare
+					// can only add if i+2 already exists, because this is the bonus for the spare
+					if (bowl + 2 < rolls.Count) {
+						frame = 10 + rolls[bowl + 2];
+						frameList.Add (frame);
+					}
+				} else {
+					frame = firstBowl + secondBowl;
+					frameList.Add (frame);
 				}
 			}
 		} 
+		return frameList;
+	}
 
+//		// My solution - NOT working for tests TG03
+//		int rollNum = 0;
+//		int total = 0;
+//		int spareTotal = 0;
+//		int strikeTotal = 0;
+//		int strikeSequence = 2;
+//		bool isStrike = false;
+//		
+//		
+//		foreach (int roll in rolls) {
+//			rollNum++;
+//
+//			if (rollNum > 21) {
+//				if (strikeTotal > 0) {
+//					strikeTotal += roll;
+//					frameList.Add (strikeTotal);
+//				} else {
+//					spareTotal += roll;
+//					frameList.Add (spareTotal);
+//				}
+//				continue;
+//			}
+//
+//			if (roll == 10) {
+//				rollNum++;
+//				isStrike = true;
+//				if (strikeSequence > 0) {
+//					strikeSequence--;
+//					strikeTotal += roll;
+//					continue;
+//				}
+//			}
+//			
+//			if (isStrike) {
+//				strikeTotal += roll;
+//				if (strikeSequence == 0) {
+//					frameList.Add (strikeTotal);
+//					strikeTotal -= 10;
+//				}
+//			}
+//			
+//			if (spareTotal > 0 && !isStrike) {
+//				spareTotal += roll;
+//				frameList.Add (spareTotal);
+//				spareTotal = 0;
+//			}
+//			
+//			total += roll;
+//			
+//			if (rollNum % 2 == 0) {
+//				if (total % 10 == 0 && total > 0) {
+//					spareTotal += total;
+//					total = 0;
+//				} else {
+//					if (strikeTotal >= 10) {
+//						frameList.Add (strikeTotal);
+//						strikeTotal -= 10;
+//						isStrike = false;
+//					}
+//					frameList.Add (total);
+//					strikeTotal = 0;
+//					strikeSequence = 2;
+//					total = 0;
+//				}
+//			}
+//		} 
 
 //		// Solution from Martin Beierl 
 //
@@ -139,6 +176,4 @@ public class ScoreMaster {
 //            }
 //        }
 
-		return frameList;
-	}
 }
